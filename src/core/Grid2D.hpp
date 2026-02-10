c37cb0e24e67e7c18d96bb32449db8198c72d456
Hydrazine <Zhf3PzewgSoGtzlofybR@protonmail.com>
2026-02-10 16:43:14 +0100

    optimized writer

--- src/core/Grid2D.hpp
+++ src/core/Grid2D.hpp
@@ -12,11 +12,12 @@ public:
   Grid2D(size_t nx, size_t ny) : nx(nx), ny(ny), A(nx * ny, 0.0) {}
 
   // manipulating grid values
-  Real Get(size_t i, size_t j) const;
-  void Set(size_t i, size_t j, Real val);
+  Real Get(size_t i, size_t j) const { return A[nx * j + i]; }
+  void Set(size_t i, size_t j, Real val) {A[nx * j + i] = val;}
 
   // utility functions
   bool InBounds(size_t i, size_t j);
   void FillRandom();
   Real InterpolateCenter(size_t iCenter, size_t jCenter, const size_t field);
 };
+
