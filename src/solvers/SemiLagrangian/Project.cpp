aefedc1a69e5c24706a71cbc5b87543c9a056601
vincent <vinc.heinrichs@gmail.com>
2026-03-29 11:01:24 +0200

    commit before pull (no major change)

--- src/solvers/SemiLagrangian/Project.cpp
+++ src/solvers/SemiLagrangian/Project.cpp
@@ -59,13 +59,15 @@ void updateVelocities(const Parameters & params,Fields2D & fields) {
     }
   }
 
-  // velocity at boundaries: if not fixed by BCs, copy from adjacent interior value
-  // TODO: a bit hacky ??
+  // velocity at boundaries: imposed Neumann B.C. dv/dn = 0
+  // TODO : is this great ?
   for (int j = 0; j < fields.u.ny; ++j) {
-    if (!(fields.Label(0, j) & Fields2D::SOLID) && !(fields.Label(0, j) & Fields2D::BC_U))
+    if (!(fields.Label(0, j) & Fields2D::SOLID) 
+        && !(fields.Label(0, j) & Fields2D::BC_U))
         fields.u.Set(0, j, fields.u.Get(1, j));
 
-    if (!(fields.Label(fields.nx - 1, j) & Fields2D::SOLID) && !(fields.Label(fields.nx - 1, j) & Fields2D::BC_U))
+    if (!(fields.Label(fields.nx - 1, j) & Fields2D::SOLID) 
+        && !(fields.Label(fields.nx, j) & Fields2D::BC_U))
         fields.u.Set(fields.u.nx - 1, j, fields.u.Get(fields.u.nx - 2, j));
 }
 
@@ -85,13 +87,15 @@ void updateVelocities(const Parameters & params,Fields2D & fields) {
     }
   }
 
-    // velocity at boundaries: if not fixed by BCs, copy from adjacent interior value
-    // TODO: a bit hacky ??
+    // velocity at boundaries: imposed Neumann B.C. dv/dn = 0
+    // TODO : is this great ?
     for (int i = 0; i < fields.v.nx; ++i) {
-      if (!(fields.Label(i, 0) & Fields2D::SOLID) && !(fields.Label(i, 0) & Fields2D::BC_V))
+      if (!(fields.Label(i, 0) & Fields2D::SOLID) 
+          && !(fields.Label(i, 0) & Fields2D::BC_V))
           fields.v.Set(i, 0, fields.v.Get(i, 1));
 
-      if (!(fields.Label(i, fields.ny - 1) & Fields2D::SOLID) && !(fields.Label(i, fields.ny - 1) & Fields2D::BC_V))
+      if (!(fields.Label(i, fields.ny - 1) & Fields2D::SOLID) 
+          && !(fields.Label(i, fields.ny - 1) & Fields2D::BC_V))
           fields.v.Set(i, fields.v.ny - 1, fields.v.Get(i, fields.v.ny - 2));
     }
 }
