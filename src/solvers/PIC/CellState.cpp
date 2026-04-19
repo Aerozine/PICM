3a86619357579118d8f421077f9de4ac832058c2
vincent <vinc.heinrichs@gmail.com>
2026-04-19 12:23:48 +0200

    small fix

--- src/solvers/PIC/CellState.cpp
+++ src/solvers/PIC/CellState.cpp
@@ -13,7 +13,6 @@ for (int i = 0; i < fields->countAliveParticles->nx; i++) {
       fields->setFluid(i + 1, j + 1);
       continue;
     }
-    // then if no solid and no particle
     fields->setAir(i + 1, j + 1);
   }
 }
@@ -22,42 +21,25 @@ for (int i = 0; i < fields->countAliveParticles->nx; i++) {
       for (int j = 0; j < ny; j++) {
         if (!IS_AIR(fields->Label(i + 1, j + 1)))
           continue;
-        // zero all 4 faces of this cell!
-        // Only zero a face if the neighbour on the other side is also non-fluid
-        // Left face u(i, j): other side is Label(i, j+1)
-        if (!IS_FLUID(fields->Label(i, j + 1)))
+       
+        labeltype leftLabel = fields->Label(i, j + 1);
+        labeltype rightLabel = fields->Label(i + 2, j + 1);
+        labeltype bottomLabel = fields->Label(i + 1, j);
+        labeltype topLabel = fields->Label(i + 1, j + 2);
+
+        // u, v = 0 at AIR|AIR or AIR|SOLID interfaces
+        if (!IS_FLUID(leftLabel))
           fields->u.Set(i, j, static_cast<varType>(0));
-        // Right face u(i+1, j): other side is Label(i+2, j+1)
-        if (!IS_FLUID(fields->Label(i + 2, j + 1)))
+        if (!IS_FLUID(rightLabel))
           fields->u.Set(i + 1, j, static_cast<varType>(0));
-        // Bottom face v(i, j): other side is Label(i+1, j)
-        if (!IS_FLUID(fields->Label(i + 1, j)))
+        if (!IS_FLUID(bottomLabel))
           fields->v.Set(i, j, static_cast<varType>(0));
-        // Top face v(i, j+1): other side is Label(i+1, j+2)
-        if (!IS_FLUID(fields->Label(i + 1, j + 2)))
+        if (!IS_FLUID(topLabel))
           fields->v.Set(i, j + 1, static_cast<varType>(0));
-        // the write race is harmless because we write 0 anyway
-
-        /*varType alive = 0.0;
-        // TODO : gerer les BC_U // BC_V // BC_P
-        // countAliveParticles is of size (nx, ny)
-        // Label is of size (nx + 2; ny + 2) so shifted by i + 1, j + 1
-        alive = fields->countAliveParticles.Get(i, j);
-
-        if (alive == 0.0){
-            //fields->ResetLabel(i + 1, j + 1, Fields2D::AIR);
-            fields->setAir(i+1, j+1);
-            // verifier les indices ici dessous
-            if(IS_AIR(fields->Label(i, j + 1)))
-                fields->u.Set(i, j, 0.0);
-            if(IS_AIR(fields->Label(i + 1, j)))
-                fields->v.Set(i, j, 0.0);
-        }
-        else{
-            //fields->ResetLabel(i + 1, j + 1, Fields2D::FLUID);
-            fields->setFluid(i+1,j+1);
-        }
-        */
+        
+        // if (IS_FLUID(leftLabel) &&  IS_FLUID(rightLabel) &&
+        //     IS_FLUID(bottomLabel) && IS_FLUID(topLabel))
+        //   fields->setFluid(i + 1, j + 1);
       }
     }
 }
